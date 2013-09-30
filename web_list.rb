144089c6de8066775b0226410b75017c28ebaafb
José Alberto Pérez Melián <japmelian@gmail.com>
2013-09-30 22:55:04 +0100

    Updating

--- web_list.rb
+++ web_list.rb
@@ -1,38 +1,42 @@
-require 'faraday'
 require 'twitter'
 require 'rack'
+require 'erb'
 require 'pry-debugger'
 require 'thin'
+require './configure'
 
-class HelloWorld
+class WebList
+  def initialize
+    @all_tweets = [] # Array where all tweets will be saved
+    @name = ''       # Username
+    @number = 0      # Number of tweets to show
+  end
+
+  def erb(template)
+    template_file = File.open("web_list.html.erb", 'r')
+    ERB.new(File.read(template_file)).result(binding)
+  end
 
   def call env
     req = Rack::Request.new(env)
-    res = Rack::Response.new 
+    
     binding.pry if ARGV[0]
-    res['Content-Type'] = 'text/html'
-    name = (req["firstname"] && req["firstname"] != '') ? req["firstname"] :'World'
-    res.write <<-"EOS"
-      <!DOCTYPE HTML>
-      <html>
-        <title>Rack::Response</title>
-        <body>
-          <h1>
-             Hello #{name}!
-             <form action="/" method="post">
-               Your name: <input type="text" name="firstname" autofocus><br>
-               <input type="submit" value="Submit">
-             </form>
-          </h1>
-        </body>
-      </html>
-    EOS
-    res.finish
+
+    @name = (req["firstname"] && req["firstname"] != '' && Twitter.user?(req["firstname"]) == true ) ? req["firstname"] : ''
+
+    @number = (req["n"] && req["n"].to_i>1 ) ? req["n"].to_i : 1
+
+    if @name == req["firstname"]
+      ultimos_t = Twitter.user_timeline(@name,{:count=>@number.to_i})
+      @todo_tweet =(@todo_tweet && @todo_tweet != '') ? ultimos_t.map{ |i| i.text} : ''       
+    end
+
+    Rack::Response.new(erb('twitter.html.erb'))
   end
 end
 
 Rack::Server.start(
-  :app => HelloWorld.new,
+  :app => WebList.new,
   :Port => 9292,
   :server => 'thin'
 )
